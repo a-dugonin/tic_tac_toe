04f6263822b9df0c30e12434c4e0df31be1edaae
a-dugonin <a_dugonin@mail.ru>
2023-12-11 19:30:46 +0300

    Исправлены замечания после ревью

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,41 +6,41 @@ WINNING_COMBINATIONS = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (
 class Board:
     """ Класс игрового поля """
 
-    def __init__(self, state_board: list['Cell'] | list = None, count_busy_cells: int = 0):
-        self.state_board = state_board
-        self.count_busy_cells = count_busy_cells
+    def __init__(self, list_board_cells: list['Cell'] | list = None, count_filled_cells: int = 0):
+        self.list_board_cells = list_board_cells
+        self.count_filled_cells = count_filled_cells
 
-    def change_state_board(self, num_cell: int, player: 'Player'):
+    def change_state_cell_board(self, num_cell: int, player: 'Player'):
         """ Метод изменяет значение ячейки """
-        if self.state_board[num_cell].cell_value == ' ':
-            self.state_board[num_cell].cell_value = player.value_for_move
-            self.count_busy_cells += 1
+        if self.list_board_cells[num_cell].cell_value == ' ':
+            self.list_board_cells[num_cell].cell_value = player.value_for_move
+            self.count_filled_cells += 1
             print('Ячейка заполнена')
         else:
             print('Не удалось изменить состояниие ячейки, т.к. она не пуста')
 
     def create_cells_for_board(self):
         """ Метод позволяет создать ячейку для игрового поля """
-        self.state_board = [Cell(index_cell) for index_cell in range(1, 10)]
+        self.list_board_cells = [Cell(index_cell) for index_cell in range(1, 10)]
 
     def check_win(self):
         """ Метод для выявления победителя """
         for combination in WINNING_COMBINATIONS:
-            if self.state_board[combination[0]].cell_value == self.state_board[combination[1]].cell_value == \
-                    self.state_board[combination[2]].cell_value != ' ':
+            if self.list_board_cells[combination[0]].cell_value == self.list_board_cells[combination[1]].cell_value == \
+                    self.list_board_cells[combination[2]].cell_value != ' ':
                 return True
         return False
 
     def __str__(self):
-        return (f'{[cell.cell_value for cell in self.state_board[0:3]]}'
-                f'\n{[cell.cell_value for cell in self.state_board[3:6]]}'
-                f'\n{[cell.cell_value for cell in self.state_board[6:9]]}')
+        return (f'{[cell.cell_value for cell in self.list_board_cells[0:3]]}'
+                f'\n{[cell.cell_value for cell in self.list_board_cells[3:6]]}'
+                f'\n{[cell.cell_value for cell in self.list_board_cells[6:9]]}')
 
 
 class Cell:
     """ Класс для создания ячейки игрового поля """
 
-    def __init__(self, cell_num, cell_value: str = ' '):
+    def __init__(self, cell_num: int, cell_value: str = ' '):
         self.cell_value = cell_value
         self.cell_num = cell_num
 
@@ -90,12 +90,16 @@ class Game:
                   f'{self.players[1]}: {self.players[1].count_win} побед')
             answer = input('Хотите сыграть еще? Введите ответ (да или нет): ').lower()
 
-    def init_game(self):
-        """ Метод для инициализации одной игры из цикла """
+    def applying_default_settings_for_init_game(self):
+        """ Метод позволяет привести игру к стартовым настройкам по умолчанию """
         shuffle(self.players)
         self.players[0].add_value_for_move('X'), self.players[1].add_value_for_move('0')
         self.board = Board()
         self.board.create_cells_for_board()
+
+    def init_game(self):
+        """ Метод для инициализации одной игры из цикла """
+        self.applying_default_settings_for_init_game()
         while not self.state_game:
             for player in self.players:
                 if self.start_move(player):
@@ -103,7 +107,7 @@ class Game:
                     print(f'Игра завершена! Победил(а) игрок {player.name}!')
                     break
                 else:
-                    if self.board.count_busy_cells == 9:
+                    if self.board.count_filled_cells == 9:
                         self.state_game = True
                         print('Игра завершена! Ничья!')
                         break
@@ -111,7 +115,7 @@ class Game:
     def start_move(self, player: 'Player'):
         """ Метод для запуска одного хода игры одним из игроков """
         num_cell = player.make_move() - 1
-        self.board.change_state_board(num_cell, player)
+        self.board.change_state_cell_board(num_cell, player)
         print(self.board)
         if self.board.check_win():
             player.count_win += 1
